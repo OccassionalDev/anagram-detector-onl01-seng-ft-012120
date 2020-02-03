b840cf4f58e90885b42faf733ff04cfdace04a8a
Erica Schimmel <ericaschimmel01@gmail.com>
2020-02-03 20:51:51 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,27 @@
 # Your code goes here!
+
+class Anagram
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end 
+  
+  def match(given_word_list)
+   list_of_anagrams = []
+   word_letters = @word.split("").sort
+   
+   #Step 1: For each given word in the array search for the word that is the same as the instance variable
+   
+   given_word_list.each do |given_word|
+     given_word_letters = given_word.split("").sort
+     
+     #Step 2: If the word matches, add it to the anagram list
+     if given_word_letters == word_letters
+       list_of_anagrams << given_word
+     end 
+   end 
+   
+    return list_of_anagrams
+  end 
+end 
