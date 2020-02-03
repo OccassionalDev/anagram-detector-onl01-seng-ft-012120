09218d1435c0d17e04bc593c9cec50f5e367e719
Erica Schimmel <ericaschimmel01@gmail.com>
2020-02-03 20:48:03 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,8 +8,18 @@ class Anagram
   end 
   
   def detector(given_word_list)
-   given_word_list.select do |letters|
+   list_of_anagrams = []
+   word_letters = @word.split("").sort
+   
+   #Step 1: For each given word in the array search for the word that is the same as the instance variable
+   
+   given_word_list.each do |given_word|
+     given_word_letters = given_word.split("").sort
      
+     #Step 2: If the word matches, add it to the anagram list
+     if given_word_letters == word_letters
+       list_anagrams << given_word
+     end 
    end 
   end 
   
