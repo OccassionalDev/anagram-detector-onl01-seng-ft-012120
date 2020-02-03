f08faa16fb847676591056c81a92c3dd90725da8
Erica Schimmel <ericaschimmel01@gmail.com>
2020-02-03 20:34:00 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,9 +7,14 @@ class Anagram
     @word = word
   end 
   
-  def detector(given_word)
+  def detector(given_word_list)
+     word_letters = @word.split("").sort
      
-    
+     
+     given_word_list.each do |word_value|
+      if word_value.split("").sort == word_letters
+        return word_value
+      end 
   end 
   
   
