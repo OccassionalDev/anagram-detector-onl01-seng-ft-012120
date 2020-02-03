ac236464511b8faff97c920fb820c9db3d1c4088
Erica Schimmel <ericaschimmel01@gmail.com>
2020-02-03 20:38:07 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -10,15 +10,7 @@ class Anagram
   def detector(given_word_list)
      word_letters = @word.split("").sort
      
-     given_word_list.each do |word_value|
-       split_value = word_value.split("").sort
-       
-      if split_value == word_letters
-        return word_value
-      end 
-    end 
-    
-    return false
+ 
   end 
   
   
